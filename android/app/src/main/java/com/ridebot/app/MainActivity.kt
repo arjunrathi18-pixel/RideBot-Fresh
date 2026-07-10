5a63c04cc08097225706deb474dad3c3771f5322
arjunrathi18-pixel <arjunrathi18@gmail.com>
2026-07-10 21:24:44 +0530

    Create MainActivity.kt

---
+++ android/app/src/main/java/com/ridebot/app/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ridebot.app
+
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+
+class MainActivity: FlutterActivity(){
+
+
+override fun configureFlutterEngine(
+flutterEngine: FlutterEngine
+){
+
+super.configureFlutterEngine(
+flutterEngine
+)
+
+
+RideBotBridge.setup(
+flutterEngine
+)
+
+
+}
+
+
+}
