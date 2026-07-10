47256322dbc6febcdde11a46509f609cca121f3e
arjunrathi18-pixel <arjunrathi18@gmail.com>
2026-07-10 21:25:23 +0530

    Create ride_listener.dart

---
+++ lib/services/ride_listener.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/services.dart';
+
+import 'filter_engine.dart';
+
+
+
+class RideListener {
+
+
+static const channel =
+MethodChannel(
+'ridebot_channel'
+);
+
+
+
+static void start(){
+
+
+channel.setMethodCallHandler(
+(call) async {
+
+
+if(call.method=="ride_detected"){
+
+
+double fare =
+call.arguments[0];
+
+
+double distance =
+call.arguments[1];
+
+
+String pickup =
+call.arguments[2];
+
+
+
+RideData ride =
+RideData(
+
+platform:"Unknown",
+
+fare:fare,
+
+distance:distance,
+
+pickup:pickup,
+
+drop:"",
+
+);
+
+
+
+FilterSettings settings =
+FilterSettings(
+
+minFare:100,
+
+minPerKm:12,
+
+maxDistance:40
+
+);
+
+
+
+bool accept =
+RideFilterEngine()
+.checkRide(
+ride,
+settings
+);
+
+
+
+return accept;
+
+
+}
+
+
+return null;
+
+
+});
+
+
+}
+
+
+}
